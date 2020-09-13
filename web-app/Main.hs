b5149c1bc4eeb4d5a54206be6620d71d7bfa2c9c
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2020-09-13 04:13:23 -0700

    Add web-server as second executable app

---
+++ web-app/Main.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Prelude
+--import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson
+import Data.Aeson.Types
+--import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+--import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+--import Lucid
+--import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+--import Text.Blaze
+--import Text.Blaze.Html.Renderer.Utf8
+import Servant.Types.SourceT (source)
+import qualified Data.Aeson.Parser
+---import qualified Text.Blaze.Html
+import Lib
+
+type UserAPI1 = "state" :> Get '[JSON] [User]
+
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  , registration_date :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  ]
+
+server1 :: Server UserAPI1
+server1 = return users1
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app1 :: Application
+app1 = serve userAPI server1
+
+main :: IO ()
+main = run 8081 app1
