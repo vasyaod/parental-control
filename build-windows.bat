bb2fbf72553df53a42f720333bd05350a1635194
Vasilii Vazhesov <vasilii.vazhesov@gmail.com>
2022-01-02 14:54:03 -0800

    fix config file

--- build-windows.bat
+++ build-windows.bat
@@ -1,6 +1,6 @@
 echo %homedrive%%homepath%
 
-copy "..\config-windows.yml" config.yml
+copy config-windows.yml config.yml
 copy %homedrive%%homepath%\AppData\Roaming\local\bin\parental-control.exe parental-control.exe
 copy %homedrive%%homepath%\AppData\Roaming\local\bin\parental-control-web.exe parental-control-web.exe
 copy windows\parental-control-starter.exe parental-control-starter.exe
